3faf3b434d2bcb95176871f2ab6f54778cd7e618
saraLaptop <kostovska898@outlook.com>
2018-01-07 00:25:18 +0100

    prikacuvanje na ime na slika kako id.extenzija vo tabelata i vo uploads

--- vnesiOglas.php
+++ vnesiOglas.php
@@ -4,27 +4,34 @@ include"header.php";
 
 if(isset($_POST['file'])){
 
-	$name = $_FILES['prikaciSlika']['name'];
+	
 	$target_dir = "uploads/";
 	$target_file = $target_dir . basename($_FILES["prikaciSlika"]["name"]);
 
-	// Select file type
+	// zemi ja ekstenzijata
 	$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-	// Valid file extensions
+	// validni ekstenzii
 	$extensions_arr = array("jpg","jpeg","png","gif");
 
 	// Check extension
 	if( in_array($imageFileType,$extensions_arr) ){
 
-		// Insert record
-		//$sql = "INSERT INTO sliki(oglasID,link) VALUES('11','$name')";
-		//$result = mysqli_query($conn,$sql);
+		// vo bazata na sliki
+		$sql = "INSERT INTO sliki(oglasID) VALUES('3')";
+		mysqli_query($conn,$sql);
+		$idSlika = mysqli_insert_id($conn); //zema go id-to na zapiso
+		$vnesenaSlika = $idSlika.".".$imageFileType; // ime na slikata e id.extenzija
+		$sql = "UPDATE sliki SET imeSlika = '$vnesenaSlika' WHERE id = '$idSlika'";
+		mysqli_query($conn,$sql);
+		
 
-		// Upload file
-		move_uploaded_file($_FILES['prikaciSlika']['tmp_name'],$target_dir.$name);
+		// smesti go fajlot vo papkata uploads
+		move_uploaded_file($_FILES['prikaciSlika']['tmp_name'],$target_dir.$vnesenaSlika);
 
 	}
+	$grad = $_POST['grad'];
+	echo $grad;
 
 }
 ?>
@@ -38,9 +45,9 @@ if(isset($_POST['file'])){
 						<tr>
 							<td style="font-size:15px;margin:5px;">Изберете Категорија</td>
 							<td>
-								<select  class="selectpicker show-tick">
-									<option>Издавање</option>
-									<option>Продажба</option>
+								<select name="kategorija" class="selectpicker show-tick">
+									<option value="izdavanje">Издавање</option>
+									<option value="prodazba">Продажба</option>
 								</select>
 							</td>
 
@@ -48,10 +55,10 @@ if(isset($_POST['file'])){
 						<tr>
 							<td style="font-size:15px;margin:5px;">Тип на недвижнина:</td>
 							<td>
-								<select  class="selectpicker show-tick">
-									<option>Стан</option>
-									<option>Куќа</option>
-									<option>Спрат од куќа</option>
+								<select name="tip_objekt" class="selectpicker show-tick">
+									<option value="Стан">Стан</option>
+									<option value="Куќа">Куќа</option>
+									<option value="Спрат од куќа">Спрат од куќа</option>
 								</select>
 							</td>
 
@@ -59,16 +66,16 @@ if(isset($_POST['file'])){
 						<tr>
 							<td style="font-size:15px;margin:5px;">Град</td>
 							<td>
-								<select  class="selectpicker show-tick">
-									<option>Скопје</option>
-									<option>Штип</option>
-									<option>Битола</option>
-									<option>Прилеп</option>
-									<option>Охрид</option>
-									<option>Струмица</option>
-									<option>Свети Николе</option>
-									<option>Кочани</option>
-									<option>Берово</option>
+								<select name="grad" class="selectpicker show-tick">
+									<option value="Скопје">Скопје</option>
+									<option value="Штип">Штип</option>
+									<option value="Битола">Битола</option>
+									<option value="Прилеп">Прилеп</option>
+									<option value="Охрид">Охрид</option>
+									<option value="Струмица">Струмица</option>
+									<option value="Свети Николе">Свети Николе</option>
+									<option value="Кочани">Кочани</option>
+									<option value="Берово">Берово</option>
 
 								</select>
 							</td>
@@ -77,16 +84,16 @@ if(isset($_POST['file'])){
 						<tr>
 
 							<td  style="font-size:15px;margin:5px;">Наслов на огласот:</td>
-							<td><input type='text' class="form-control"></td>
+							<td><input type='text' name="naslov" class="form-control"></td>
 						</tr>
 						<tr>
 							<td style="font-size:15px;margin:5px;">Опис:</td>
-							<td ><input type='text' class="form-control"></td>
+							<td ><input type='text' name = 'opis' class="form-control"></td>
 						</tr>
 
 						<tr>
 							<td style="font-size:15px;margin:5px;">Квадратура:</td>	
-							<td ><input type='text' class="form-control"></td>	
+							<td ><input type='text' name="kvadratura" class="form-control"></td>	
 
 						</tr>
 						<tr>
@@ -94,10 +101,10 @@ if(isset($_POST['file'])){
 								Ентриер:
 							</td>
 							<td>
-								<select  class="selectpicker show-tick">
-									<option>наместен</option>
-									<option>делумно наместен</option>
-									<option>ненаместен</option>
+								<select  name="enterier" class="selectpicker show-tick">
+									<option value ="наместен" >наместен</option>
+									<option value="делумно наместен">делумно наместен</option>
+									<option value="ненаместен">ненаместен</option>
 
 								</select>	
 							</td>	
@@ -105,16 +112,16 @@ if(isset($_POST['file'])){
 						<tr>
 							<td style="font-size:15px;margin:5px;">Соби:</td>
 							<td>
-								<select  class="selectpicker show-tick">
-									<option>1</option>
-									<option>2</option>
-									<option>3</option>
-									<option>4</option>
-									<option>5</option>
-									<option>6</option>
-									<option>7</option>
-									<option>8</option>
-									<option>над 9</option>
+								<select name="brSobi" class="selectpicker show-tick">
+									<option value="1" >1</option>
+									<option value="2">2</option>
+									<option value="3">3</option>
+									<option value="4">4</option>
+									<option value="5">5</option>
+									<option value="6">6</option>
+									<option value="7">7</option>
+									<option value="8">8</option>
+									<option value="над 9">над 9</option>
 
 								</select>	
 							</td>	
@@ -122,36 +129,36 @@ if(isset($_POST['file'])){
 						<tr>
 							<td style="font-size:15px;margin:5px;">Греење:</td>
 							<td>
-								<select  class="selectpicker show-tick">
-									<option>Нема</option>
-									<option>Централно</option>
-									<option>Струја</option>
-									<option>Дрва</option>
-									<option>Друго</option>								
+								<select  name = 'greenje' class="selectpicker show-tick">
+									<option value="Нема">Нема</option>
+									<option value="Централно">Централно</option>
+									<option value="Струја">Струја</option>
+									<option value="Дрва" >Дрва</option>
+									<option value="Друго">Друго</option>								
 								</select>	
 							</td>	
 						</tr>
 						<tr>
 							<td style="font-size:15px;margin:5px;">Спрат:</td>	
-							<td ><input type='text' class="form-control"></td>	
+							<td ><input type='text'name="Спрат" class="form-control"></td>	
 
 						</tr>
 						<tr>
 							<td style="font-size:15px;margin:5px;">Вкупно спратови:</td>	
-							<td ><input type='text' class="form-control"></td>	
+							<td ><input type='text'name='Вкупно спратови' class="form-control"></td>	
 
 						</tr>
 						<tr>
 							<td style="font-size:15px;margin:5px;">Година на изградба:</td>	
-							<td ><input type='text' class="form-control"></td>	
+							<td ><input type='date' name="godIzgradba"  class="form-control" ></td>	
 
 						</tr>
 						<tr>
 							<td style="font-size:15px;margin:5px;">Лифт:</td>
 							<td>
-								<select  class="selectpicker show-tick">
-									<option>Да</option>
-									<option>Не</option>							
+								<select name="lift" class="selectpicker show-tick">
+									<option value="Да">Да</option>
+									<option value="Не">Не</option>							
 								</select>	
 							</td>	
 						</tr>
@@ -159,10 +166,10 @@ if(isset($_POST['file'])){
 							<td style="font-size:15px;margin:5px;">Цена:</td>	
 							<td ><input type='text' class="form-control"></td>
 							<td>
-								<select  class="selectpicker show-tick">
-									<option>Евра</option>
-									<option>Денари</option>	
-									<option>По договор</option>						
+								<select name="cena" class="selectpicker show-tick">
+									<option value="Евра">Евра</option>
+									<option value="Денари">Денари</option>	
+									<option value="По договор">По договор</option>						
 								</select>	
 							</td>		
 
