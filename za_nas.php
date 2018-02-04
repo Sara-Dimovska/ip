ce2242c35d89843a1016817bed216959a8704cce
ivanaaaa <35149883+ivanaaaa@users.noreply.github.com>
2018-02-04 17:13:45 +0100

    little more responsive

--- za_nas.php
+++ za_nas.php
@@ -1,57 +1,58 @@
 <?php include "nenajaveniHeader.php"; ?>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
-	<head>
-		<link rel="stylesheet" type="text/css" href="style.css">
-	</head>
-	<body>
-		<div class="container" style="margin-left:200px;margin-top:30px;margin-bottom:50px;background-color:whitesmoke;border-radius:4px;padding-left: 40px;
-									  padding-right: 40px;">
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="container" style="margin-top:30px;margin-bottom:50px;background-color:whitesmoke;border-radius:4px;">
 
-			<div style="margin:50 100 50 200;">
-				<video width="700" controls>
-					<source src="assets/VIDEO.mp4" type="video/mp4">
+    <div class="row">
+        <div class="col-xs-12 col-md-12 text-center" style="padding-top: 5vh">
+            <video width="700" controls>
+                <source src="assets/VIDEO.mp4" type="video/mp4">
 
-				</video>
-			</div>
+            </video>
+        </div>
+    </div>
 
+    <div class="row">
+        <div class="col-xs-12 col-md-12 text-center">
+            <h3 style='color:#dd6464;font-size: 35px;'>Тимот на &quot;Смести се&quot;</h3>
+            <div class="col-xs-4 col-md-4">
+                <div id='person'>
+                    <img src="assets/sara.png" style="height:250px;width:250px;">
+                    <p><span style="font-size:20px;color:#dd6464;">Сара Костовска</span></p>
+                    <span style="font-style: italic; font-size:18px;">
+                                full stack developer, голем придонес во дизајнот на апликацијата, одговорна за SQL упити и управување со податоци, кодира во php back end јазик. Анти-одлагање и се стреми да заврши квалитетна работа во пристоен временски период. Посветена да се развие во што подобар програмер а нејзините клиенти се секогаш задоволни.
+                            </span>
+                </div>
+            </div>
+            <div class="col-xs-4 col-md-4">
+                <div id='person'>
+                    <img src="assets/ace.png" style="height:250px;width:250px;">
+                    <p><span style="font-size:20px;color:#dd6464;">Александар Милановски</span></p>
+                    <span style="font-style: italic; font-size:18px;">
+                                графички дизајнер и може да работи на различни активности. Одговорен за изработка на видео и слики. Како системски и веб аналитичар  им помага на програмерите да ги интерпертираат потребните барања и да одговараат на нив. Со својот креативен дух придонесува за развојот на добри идеи.
+                            </span>
+                </div>
+            </div>
+            <div class="col-xs-4 col-md-4">
+                <div id='person'>
+                    <img src="assets/ivana.png" style="height:250px;width:250px;">
+                    <p><span style="font-size:20px;color:#dd6464;">Ивана Цебова</span></p>
+                    <span style="font-style: italic; font-size:18px;">
+                                одговорен менаџер и full stack developer се грижи за имплементација на безбедни и коректни функционалности, придонесува во менаџирањето на базата на податоци и кодира во php.Таа е одличен иднивидуален програмер, но уште подобра во тим. Нејзината програмерска страст континуиарно е поттикната од желбата за учење и имплементација на нови идеи.
+                            </span>
+                </div>
+            </div>
+        </div>
+    </div>
 
+</div>
 
-			<h3 style='color:#dd6464;    font-size: 35px;
-					   margin-left: 350px;'>Тимот на &quot;Смести се&quot;</h3>		
-
-			<div style="display: inline-flex; margin-bottom:50px;">
-				<div id='person'>
-					<img src="assets/sara.png" style="height:250px;width:250px;"> 
-					<p ><center style="font-size:20px;color:#dd6464;">Сара Костовска</center></p>
-					<center style="font-style: italic; font-size:18px;">
-						full stack developer, голем придонес во дизајнот на апликацијата, одговорна за SQL упити и управување со податоци, кодира во php back end јазик. Анти-одлагање и се стреми да заврши квалитетна работа во пристоен временски период. Посветена да се развие во што подобар програмер а нејзините клиенти се секогаш задоволни.
-					</center>
-				</div>
-				<div id='person'>
-					<img src="assets/ace.png" style="height:250px;width:250px;">
-					<p ><center style="font-size:20px;color:#dd6464;">Александар Милановски</center></p>
-					<center style="font-style: italic; font-size:18px;">
-						графички дизајнер и може да работи на различни активности. Одговорен за изработка на видео и слики. Како системски и веб аналитичар  им помага на програмерите да ги интерпертираат потребните барања и да одговараат на нив. Со својот креативен дух придонесува за развојот на добри идеи. 
-					</center>
-				</div>
-				<div id='person'>
-					<img src="assets/ivana.png" style="height:250px;width:250px;">
-					<p ><center style="font-size:20px;color:#dd6464;">Ивана Цебова</center></p>
-					<center style="font-style: italic; font-size:18px;">
-						одговорен менаџер и full stack developer се грижи за имплементација на безбедни и коректни функционалности, придонесува во менаџирањето на базата на податоци и кодира во php.Таа е одличен иднивидуален програмер, но уште подобра во тим. Нејзината програмерска страст континуиарно е поттикната од желбата за учење и имплементација на нови идеи.
-					</center>
-				</div>
-
-			</div>			  					  
-
-		</div>
-
-	</body>
-	<footer class="panel-footer">
-		<center>		
-			<h4>COPYRIGHT 	&copy; SMESTI-SE.МК 2018</h4>
-			<a href="pravila.php">ПРАВИЛА И УСЛОВИ</a>
-		</center>
-
-	</footer>
+</body>
+<footer class="panel-footer text-center">
+        <h4>COPYRIGHT &copy; SMESTI-SE.МК 2018</h4>
+        <a href="pravila.php">ПРАВИЛА И УСЛОВИ</a>
+</footer>
 </html>
